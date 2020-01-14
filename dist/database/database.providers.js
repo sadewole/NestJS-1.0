e19ba8cfbd168af6ddd505bee8df2015dd4bc671
Samuel Adewole <samueladewole15@gmail.com>
2020-01-14 02:41:47 +0100

    prepare for production

--- dist/database/database.providers.js
+++ dist/database/database.providers.js
@@ -1,15 +1,15 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 const mongoose = require("mongoose");
-exports.databaseProviders = [
-    {
-        provide: 'DATABASE_CONNECTION',
-        useFactory: () => mongoose.connect('mongodb+srv://sam-nest:nest@cluster0-2smzw.mongodb.net/test?retryWrites=true&w=majority', {
-            useNewUrlParser: true,
-            useFindAndModify: false,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        })
-    }
-];
+exports.databaseProviders = [{
+    provide: 'DATABASE_CONNECTION',
+    useFactory: () => mongoose.connect(process.env.MongoURI, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+}];
 //# sourceMappingURL=database.providers.js.map

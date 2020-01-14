bd0d88193f637018f00043e75e2e9d94a0b5172d
Samuel Adewole <samueladewole15@gmail.com>
2020-01-14 02:04:34 +0100

    clean up - prepare for production

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,6 +4,11 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(process.env.MongoURI)
+      mongoose.connect(process.env.MongoURI,  {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
   }
 ];
